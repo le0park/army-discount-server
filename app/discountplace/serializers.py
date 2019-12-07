0efb1e417f70d26d4c212df0e2ede47155f92759
Junyoung Park <carpe0308@naver.com>
2019-12-07 21:18:33 +0900

    add likes field on PlaceSerializer

--- app/discountplace/serializers.py
+++ app/discountplace/serializers.py
@@ -1,10 +1,27 @@
 from rest_framework import serializers
 from .models import *
 
-class PlaceSerializer(serializers.ModelSerializer):
+class CustomSerializer(serializers.ModelSerializer):
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(CustomSerializer, self).get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, 'extra_fields', None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
+
+
+class PlaceSerializer(CustomSerializer):
+    likes = serializers.SerializerMethodField()
+
+    def get_likes(self, obj):
+        return obj.like_set.count()
+
     class Meta:
         model = Place
         fields = '__all__'
+        extra_fields = ['likes']
+
 
 
 class LikeSerializer(serializers.ModelSerializer):
